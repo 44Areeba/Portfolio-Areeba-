42b79305898e82c5502edcfb50cf82db5b40f8ef
Areeba_Khan44 <98646330+44Areeba@users.noreply.github.com>
2023-02-25 15:39:10 +0500

    Add files via upload

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,53 @@
+import pro1 from"../assets/project1.png";
+import pro2 from"../assets/project2.png";
+import pro3 from"../assets/project3.png";
+
+import pro4 from"../assets/project1.png";
+import pro5 from"../assets/project2.png";
+import pro6 from"../assets/project3.png";
+
+
+const ProjectCardData=[
+    {
+    imgsrc:pro1,
+    title:"Ecommerce website Design",
+    text:"user friendly page and eye catching website",
+    view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+    },
+    {
+        imgsrc:pro2,
+        title:"LMS Course Website",
+        text:"user friendly page and eye catching website",
+        view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+    },
+    {
+            imgsrc:pro3,
+            title:"Food Website",
+            text:"user friendly page and eye catching website",
+            view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+    },
+
+    {
+        imgsrc:pro4,
+        title:"Ecommerce website Design",
+        text:"user friendly page and eye catching website",
+        view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+        },
+        {
+            imgsrc:pro5,
+            title:"LMS Course Website",
+            text:"user friendly page and eye catching website",
+            view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+        },
+        {
+                imgsrc:pro6,
+                title:"Food Website",
+                text:"user friendly page and eye catching website",
+                view:"https://www.youtube.com/watch?v=P8YuWEKTeuE",
+        },
+    
+
+]
+
+
+export default ProjectCardData;
